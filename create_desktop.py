25c64ad7d9852b5bae9527964ee8144720ae6a1b
Truthwatcher64 <49875526+burgeale002@users.noreply.github.com>
2023-08-15 12:50:48 -0600

    Update create_desktop.py

--- create_desktop.py
+++ create_desktop.py
@@ -9,7 +9,7 @@ def install_method():
     text_sequence=text_sequence+"Exec=/usr/bin/python3 /home/"+holder+"/.password_engine/window_launcher.py\n"
     text_sequence=text_sequence+"Name=Password Engine\n"
     text_sequence=text_sequence+"Icon=/home/"+holder+"/.password_engine/icon.png\n"
-    with open("/home/"+holder+"/Downloads/Password_Engine_Master/password_engine.desktop", "w") as file:
+    with open("/home/"+holder+"/Downloads/Password_Engine-main/password_engine.desktop", "w") as file:
         file.write(text_sequence)
 
 def uninstall_method():
@@ -18,7 +18,7 @@ def uninstall_method():
     #create an uninstall script (uninstall_linux.sh)
     text_sequence="rm /home/"+holder+"/.local/share/applications/password_engine.desktop\n"
     text_sequence=text_sequence+"rm -r /home/"+holder+"/.password_engine\n"
-    with open("/home/"+holder+"/Downloads/Password_Engine_Master/uninstall_linux.sh", "w") as file:
+    with open("/home/"+holder+"/Downloads/Password_Engine-main/uninstall_linux.sh", "w") as file:
         file.write(text_sequence)
 
 
