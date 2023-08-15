5f0410a1eb87e5e0d08cbbd53112cdbc0688f601
Truthwatcher64 <49875526+burgeale002@users.noreply.github.com>
2023-08-15 12:48:55 -0600

    Update create_desktop.py

--- create_desktop.py
+++ create_desktop.py
@@ -1,15 +1,26 @@
 import os
 
-def one_method():
+def install_method():
     holder=os.getlogin()
 
-    #Create the .desktop
+    #Create the .desktop file (password_engine.desktop)
     text_sequence="[Desktop Entry]\n"
-    text_sequence.__add__("Terminal=false\nType=Application")
-    text_sequence.__add__("Exec=/usr/bin/python3 /home/"+holder+"/.password_engine/window_launcher.py")
-    text_sequence.__add__("Name=Password Engine")
-    text_sequence.__add__("Icon=/home/"+holder+"/.password_engine/icon_square.png")
-    with open("home/"+holder+"/Downloads/Password_Engine_Master/password_engine.desktop", "w") as file:
+    text_sequence=text_sequence+"Terminal=false\nType=Application\n"
+    text_sequence=text_sequence+"Exec=/usr/bin/python3 /home/"+holder+"/.password_engine/window_launcher.py\n"
+    text_sequence=text_sequence+"Name=Password Engine\n"
+    text_sequence=text_sequence+"Icon=/home/"+holder+"/.password_engine/icon.png\n"
+    with open("/home/"+holder+"/Downloads/Password_Engine_Master/password_engine.desktop", "w") as file:
         file.write(text_sequence)
 
-one_method()
+def uninstall_method():
+    holder=os.getlogin()
+
+    #create an uninstall script (uninstall_linux.sh)
+    text_sequence="rm /home/"+holder+"/.local/share/applications/password_engine.desktop\n"
+    text_sequence=text_sequence+"rm -r /home/"+holder+"/.password_engine\n"
+    with open("/home/"+holder+"/Downloads/Password_Engine_Master/uninstall_linux.sh", "w") as file:
+        file.write(text_sequence)
+
+
+install_method()
+uninstall_method()
